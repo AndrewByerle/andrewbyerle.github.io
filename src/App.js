6489566c6bc9d367ab229377d9464ab48774dfd5
Andrew Byerle <andran@live.unc.edu>
2021-02-17 00:16:35 -0500

    updated about me

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import { Header, Footer } from "./components";
-import ReactMarkdown from "react-markdown";
-import { lesson_one } from "./data/markdown";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { GrInstagram } from "react-icons/gr"
+import { FiSend } from "react-icons/fi"
 
-const intro = (<p1 class="intro">Hello, I'm <b>Andrew!</b> <br></br> I am a sophomore at UNC-Chapel Hill studying Computer Science and Music</p1>)
+
+
+const intro = (<p1 class="intro"><span>Hi, I'm <p2 class="name">Andrew!</p2></span> <span> I am a sophomore at UNC-Chapel Hill <span>studying Computer Science and Music.</span></span></p1>)
+const About = (<p2 class="about"><span>Hey! I'm a sophomore at the University of North Carolina at Chapel Hill following my passions for application development and music. I am currently from Northern Virginia (Nova), but I moved frequenly in a military family growing up.Some of the places I'v lived include Germany, Utah, Nebraska, Maryland, Arizona, Alabama, and Michigan.</span></p2>)
+const About2 = (<p2 class="about" id="about2">
+                <div>
+                this is the second part of my paragraph.
+                </div>
+               </p2>)
+               
+const blank_space = (<br></br>)
+const socials = (<div class="socials"> 
+                    <a class="github" href="https://github.com/AndrewByerle"> 
+                      <FaGithub size="3em" /> 
+                    </a>
+                    <a class="mail" href="mailto: andran@live.unc.edu">
+                      <FiSend size="3em" />  
+                    </a> 
+                    <a class="linkedin" href="https://www.linkedin.com/in/andrew-byerle/">
+                      <FaLinkedinIn size="3em" />
+                    </a> 
+                    <a class="insta" href="https://www.instagram.com/andrew_byerle/">
+                      <GrInstagram size="3em" />
+                    </a>
+                 </div>) 
 
 
 const App = () => {
@@ -12,145 +37,25 @@ const App = () => {
       {/* <Header /> */}
       </div>
       <div id="content">
-        <body>
-          <p1>
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-          {intro}
-
-          </p1>
-        </body>
+        <body class="body">
+          <div>
+            <p1>
+              {blank_space}{blank_space}{blank_space}{blank_space}{blank_space}{blank_space}{blank_space}{blank_space}{blank_space}{blank_space}
+              <img id="self" src="/images/self.JPG"/> 
+              {intro}
+            </p1>
+          </div>
+        <div>
+          <h3 id="aboutme">
+            About me 
+          </h3>
+          <div class="aboutme">
+            <img id="fam" src="/images/fam.JPG"/>    
+            {About} {About2}           
+          </div>
+        </div>
+      </body>
+        {socials}
       </div>
       {/* <Footer /> */}
     </div>
--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-// function myFunction() {
-//   var x = document.getElementById("myTopnav");
-//   if (x.className === "topnav") {
-//     x.className += " responsive";
-//   } else {
-//     x.className = "topnav";
-//   }
-// }
-
 const Header = () => (
   <div id="scroll">  
   <header className="masthead">
     <div>
       <div>
       <Link to="/">
-      <h1 className="site-title">Andrew Byerle
+      <h1 className="title" onClick="window.location.reload();"><p1 id="title_name">Andrew Byerle</p1>
         {/* Top Navigation bar */}
         <Link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
         <p class="topnav" id="myTopnav">
             <a class="active" href="#home">Home</a>
             <a href="#about">About Me</a>
             <a href="#code">Code</a>
-            <a href="#more">More</a>
-            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-              <i class="fa fa-bars"></i>
-            </a>
+            <a href="#contact">Contact</a>            
           </p>
         </Link>
         </h1>
