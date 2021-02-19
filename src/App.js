430b584b3642bd5f920c26643899f6443405d666
Andrew Byerle <andran@live.unc.edu>
2021-02-19 01:10:10 -0500

    added contact

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import { FiSend } from "react-icons/fi"
 
 
 const intro = (<p1 class="intro"><span>Hi, I'm <p2 class="name">Andrew!</p2></span> I am a sophomore at UNC-Chapel Hill <span>studying Computer Science and Music.</span></p1>)
-const About = (<p2 class="about"><b>Hey!</b>I'm a sophomore at the University of North Carolina at Chapel Hill following my passions for application development and music. On campus, I channel my creative energy into project development for UNC's Google Develeper Student Club and solving technical problems in my community for CS + Social Good. I have also played the piano for as long as I can remember, and I am an active clarinetist in the UNC Clarinet Studio and Wind Ensemble.    </p2>)
+const About = (<p2 class="about"><b>Hey!</b>I'm a sophomore at the University of North Carolina at Chapel Hill following my passions for application development and music. In my free time, I'm an avid golfer, a skilled tennis player, a talented musician, and a proficient beginner at longboarding. On campus, I channel my creative energy into project development for UNC's Google Develeper Student Club and solving technical problems in my community for CS + Social Good. I have played piano for as long as I can remember, and I am a dedicated clarinetist in the UNC Clarinet Studio and Wind Ensemble. </p2>)
 const About2 = (<p2 class="about">
                 <div class="about2">
                   I am from Northern Virginia (NoVa), but I've traveled to many different places growing up as a military brat. Some of the places I've lived include Germany, Utah, Nebraska, Maryland, Arizona, Alabama, and Michigan.
                 </div>
                </p2>)
+const contact = (<p2 class="about"> I'd love to connect! <br></br>You can reach me below <FaArrowDown></FaArrowDown></p2>)
 const socials = (<div class="socials"> 
                     <a class="mail" href="mailto: andran@live.unc.edu">
                       <FiSend size="3em" />  
@@ -71,6 +72,10 @@ const App = () => {
         </div>
         </body>
         <div id="socials"> 
+          <h3 id="bottomheader">
+            Contact
+          </h3>
+          {contact}
           {socials}
         </div>
       </div>
